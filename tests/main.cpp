262780877d412a1ece6c057eefa2046f5d29f4d5
ThatChillProgrammer <bite74034@gmail.com>
2025-10-09 21:38:25 -0700

    pain

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,48 +1,87 @@
 #include <iostream>
+#include <unordered_map>
 #include <cstdint>
 #include <fstream>
 #include <vector>
 #include <string>
 
+#include <sstream>
+
 using namespace std;
 
-string map = 
-"     "
-" @@@ "
-" @ @ "
-" @@@ "
-"     ";
 
-int map_width = 5;
-int map_height = 5;
-
-u_int8_t color2_uint8 (int r, int g, int b) {
-
+string rgb2hex (vector <int> rgb) {
+    vector<char> hex = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+    string hexCode;
+    for (int i=0;i<3;i++){ hexCode += hex[int(rgb[i]/16)]; hexCode += hex[(rgb[i]%16)]; }
+    return hexCode;
 }
-
-void draw_ppm_map (u_int16_t h, u_int16_t w, vector<u_int16_t> img, string map) {
+vector<u_int8_t> hex2rgb (string hexCode) {
+    unordered_map<char,u_int8_t> hex = {{'0',0},{'1',1},{'2',2},{'3',3},{'4',4},{'5',5},{'6',6},
+    {'7',7},{'8',8},{'9',9},{'a',10},{'b',11},{'c',12},{'d',13},{'e',14},{'f',15}};
+    vector<u_int8_t> rgb(3);
+    for (int i=0;i<3;i++) { rgb[i] = u_int8_t((hex[hexCode[i*2]]*16)+hex[hexCode[i*2+1]]); }
+    return rgb;
+}
+void save_map (int h, int w, vector<vector<u_int8_t>> *img) {
     ofstream ofs ("map.ppm", ios::binary);
     ofs << "P6\n" << h << " " << w << "\n255\n";
-    vector<uint8_t> rgb_ppm = {255,0,0}; //rgb[0]=red rgb[1]=blue rgb[2]=green
-    vector<uint8_t> rgb_map_rectangles = {0,0,255};
-    float adder = 0;
-    int map_x=0;
-    int map_y=0;
-    for (int i=0;i<size(img);i++) {ofs << rgb_ppm[0] << rgb_ppm[1] << rgb_ppm[2];}
+    for (int i=0;i<h*w;i++) { ofs << (*img)[i][0] << (*img)[i][1] << (*img)[i][2]; }
     ofs.close();
 }
 
 
-int main(){
-    u_int16_t height = 8;
-    u_int16_t width = 8;
+//fix
+void draw_map (int window_height, int window_width, int map_height, int map_width, vector<vector<u_int8_t>>* img, string* map) {
+    vector<u_char> map_color = {255,255,255};
+    int pixel_size = window_width/map_width;
 
-    vector<u_int16_t> framebuffer(height*width, 0);
-    cout<<size(framebuffer)<<endl;
-    draw_ppm_map(height,width,framebuffer,map);
-    cout<<map.size()<< " " << map_height*map_width << endl;
-    
-    u_int8_t color = color2_uint8(255,0,0)
-    cout << 
+    int counter =0;
+    for (int i=0;i<map_height;i++) {
+
+        for (int j=0;j<map_width;j++) {
+
+            if ((*map)[counter] == '@'){
+                cout << "@: " << (window_height*pixel_size + i*pixel_size) <<  endl;
+                (*img)[(window_height*pixel_size) + i] = map_color;
+
+            }
+            counter++;
+        }
+
+    }
+}
+  
+/*
+for (int i=0;i<size(*img);i+=pixel_size) {
+        if ((*map)[count] == '@'){
+            if((*img)[i] != map_color) {
+                (*img)[i] = map_color;
+                (*img)[i+1] = map_color;
+                (*img)[i+map_width] = map_color;
+                (*img)[i+map_width+1] = map_color;
+            }
+        }       
+        count++;    
+    }
+*/
+
+int main(){
+    string map = 
+        "     "
+        " @@@ "
+        " @ @ "
+        " @@@ "
+        "     ";
+    int map_width = 5;
+    int map_height = 5;
+
+
+    int window_height = 10;
+    int window_width = 10;
+    vector<vector<u_int8_t>> framebuffer(window_height*window_width, {255,0,0});
+    draw_map(window_height,window_width,map_height,map_width,&framebuffer,&map);
+    save_map(window_height,window_width,&framebuffer);
+
     return 0;
 }
