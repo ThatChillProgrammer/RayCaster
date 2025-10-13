1fb6319ddd0284c04bbbb3e366c464293ea3daa0
ThatChillProgrammer <bite74034@gmail.com>
2025-10-12 23:59:56 -0700

    :[

--- tests/main.cpp
+++ tests/main.cpp
@@ -23,8 +23,13 @@ vector<u_int8_t> hex2rgb (int hexCode) {
     ret_vector[2] = u_int8_t(hexCode - ((ret_vector[0]<<16) + ((ret_vector[1]<<8))));
     return ret_vector;
 }
-void save_map (int h, int w, vector<vector<int>> *img) {
-    ofstream ofs ("map.ppm", ios::binary);
+void save_map (int h, int w, vector<vector<int>> *img,string number) {
+    string map = "../images/map000.ppm";
+    map[13] = number[0];
+    map[14] = number[1];
+    map[15] = number[2];
+    //string map = "map2.ppm";
+    ofstream ofs (map, ios::binary);
     ofs << "P6\n" << h << " " << w << "\n255\n";
     for (int i=0;i<h;i++) { 
         for (int j=0;j<w;j++) { vector<u_int8_t> temp = hex2rgb((*img)[i][j]); ofs << temp[0] << temp[1] << temp[2]; }
@@ -40,25 +45,25 @@ void save_map1 (int h, int w, vector<vector<int>> *img) {
     ofs.close();
 }
 void draw_map (int window_height, int window_width, int map_height, int map_width, vector<vector<int>>* img, vector<string>* map) {
+    unordered_map<char,int> colors = {{'0',rgb2hex(255,255,255)},{'1',rgb2hex(0,255,255)},{'2',rgb2hex(0,255,0)},{'3',rgb2hex(0,0,255)},{'4',rgb2hex(255,0,255)}};
     int scaling = window_width/map_height;     
     int white = rgb2hex(255,255,255);
     int blue = rgb2hex(0,0,255);
     for(int row=0;row<map_height;row++) {
         for(int col=0;col<map_width;col++) {
-            if ((*map)[row][col] != ' ') {
-                (*img)[row*scaling][col*scaling] = white;
-                if((*map)[row][col] == '1'){(*img)[row*scaling][col*scaling] = blue;}
+            if (colors[(*map)[row][col]]) {
+                (*img)[row*scaling][col*scaling] = colors[(*map)[row][col]];
                 for(int i=0;i<scaling;i++) {
                     for(int j=0;j<scaling;j++) {
-                        (*img)[row*scaling+i][col*scaling+j] = white;
-                        if((*map)[row][col] == '1'){(*img)[row*scaling+i][col*scaling+j] = blue;}
+                        (*img)[row*scaling+i][col*scaling+j] = colors[(*map)[row][col]];
                     }
                 }
             }
         }
     }
 }
-vector<vector<int>> draw_entity (float ent_x,float ent_y,float ent_a,int window_height, int window_width,vector<vector<int>>* img) {
+vector<vector<float>> draw_entity (float ent_x,float ent_y,float ent_a,int window_height, int window_width,vector<vector<int>>* img,vector<vector<int>>* map) {
+    unordered_map<int,int> colors = {{rgb2hex(255,255,255),rgb2hex(124,124,255)},{rgb2hex(0,255,255),rgb2hex(0,255,255)},{rgb2hex(0,255,0),rgb2hex(0,255,0)},{rgb2hex(0,0,255),rgb2hex(0,0,255)},{rgb2hex(255,0,255),rgb2hex(255,0,255)}};
     float fov = 30;
     float COORDINATES_MAX = 64;
     float scaling = window_height/COORDINATES_MAX;
@@ -68,30 +73,25 @@ vector<vector<int>> draw_entity (float ent_x,float ent_y,float ent_a,int window_
 
     int cur_e_x = ent_x*scaling;
     int cur_e_y = ent_y*scaling;
-    cout << cur_e_x << " " << cur_e_y << endl;
-    vector<int> distance = {0,0};
+    //cout << cur_e_x << " " << cur_e_y << endl;
+    vector<float> distance = {0,0};
     float component_x = 0;
     float component_y = 0;  
 
     float fov_bound = ent_a+fov;
     int count=0;
 
-    vector<vector<int>> distances;
-    for (int i=0;i<512;i++){
-        for (int j=0;j<512;j++){
+    vector<vector<float>> distances;
+    for (int i=0;i<window_height;i++){
+        for (int j=0;j<window_width;j++){
             count++;
-            if (hex2rgb((*img)[(component_x*scaling)+cur_e_y][(component_y*scaling)+cur_e_x])==white ) {
-                distance[1] = rgb2hex(0,255,255);
+            if (colors[(*map)[(component_x*scaling)+cur_e_y][(component_y*scaling)+cur_e_x]] ) {
+                distance[1] = colors[(*img)[(component_x*scaling)+cur_e_y][(component_y*scaling)+cur_e_x]];
                 distances.push_back(distance);
                 break;
-            }
-            if (hex2rgb((*img)[(component_x*scaling)+cur_e_y][(component_y*scaling)+cur_e_x])==blue){
-                distance[1] = rgb2hex(0,0,255);
-                distances.push_back(distance);
-                break;
-            }
+            } 
             (*img)[(component_x*scaling)+cur_e_y][(component_y*scaling)+cur_e_x]=color;
-            distance[0]+=1;
+            distance[0]+=0.1;
             component_x = distance[0]*sin(fov_bound * M_PI/180);
             component_y = distance[0]*cos(fov_bound * M_PI/180);
         }
@@ -102,11 +102,11 @@ vector<vector<int>> draw_entity (float ent_x,float ent_y,float ent_a,int window_
     }
     return distances;
 }
-
-void draw_3d (int height, int width, vector<vector<int>>* distances, vector<vector<int>>* img) {
+void draw_3d (int height, int width, vector<vector<float>>* distances, vector<vector<int>>* img) {
     int color = rgb2hex(255,0,0);
     int othCOl = rgb2hex(255,255,255);
-    int reverse_list = 511;
+    int reverse_list = size(*distances)-1;
+    //cout << height << " " << (*distances)[reverse_list][0] << " " << reverse_list << endl;
     for (int i=0;i<width;i++) {
         int object_size = (height/(*distances)[reverse_list][0])*3;
         int start_drawing = (height-object_size)/2;
@@ -144,9 +144,9 @@ int main(){
         "0       1      0",
         "2       1      0",
         "0       0      0",
-        "0 0000000      0",
+        "0 0000444      0",
         "0              0",
-        "1111122222200000"
+        "1111122222233000"
     };
     int map_height = size(vectorMap);
     int map_width = vectorMap[0].size();
@@ -159,29 +159,53 @@ int main(){
         for (int col=0;col<(window_width);col++) { framebuffer[row][col] = rgb2hex(255,0,0); }
     }
     draw_map(window_height,window_width,map_height,map_width,&framebuffer,&vectorMap);
+    vector<vector<int>> Map = framebuffer;
 
-    float player_x = 55;
-    float player_y = 55;
-    float player_angle = 180;
+    float player_x = 15;
+    float player_y = 15;
+    float player_angle = 90;
+    //vector<vector<float>> distances = draw_entity(player_x,player_y,player_angle,window_height,window_width,&framebuffer,&Map);
+    //save_map1(window_height,window_width,&framebuffer);
+    //draw_3d(window_height,window_width,&distances,&framebuffer);
+    //save_map(window_height,window_width,&framebuffer,"000");
+
+
 
     
+    string number = "";
+    string pass2func = "";
+    vector<vector<int>> distances;
+    auto start = chrono::high_resolution_clock::now();
+    for (int i=0;i<360;i++) {
+        vector<vector<float>> distances = draw_entity(player_x,player_y,player_angle,window_height,window_width,&framebuffer,&Map);
+        draw_3d(window_height,window_width,&distances,&framebuffer);
+        if (i>=100){number += int2char[(i/100)];}
+        if (i>=10){number += int2char[(i/10)%10];}
+        if (i>=0){number += int2char[(i%10)];}
+        if (number.size()==1){
+            pass2func = "00";
+            pass2func += number;
+        }
+        if (number.size()==2){
+            pass2func = "0";
+            pass2func += number;
+        }
+        if (number.size()==3){pass2func += number;}
+        //save_map(window_height,window_width,&framebuffer,pass2func);
+        //cout << number << endl;
+        if (player_angle==360){player_angle=0;}
+        player_angle +=1;
+        number ="";
+        pass2func="";
+    }   
+
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
+    cout << duration.count();
+    
+
 
     
-    //auto start = chrono::high_resolution_clock::now();
-    vector<vector<int>> distances = draw_entity(player_x,player_y,player_angle,window_height,window_width,&framebuffer);
-    //for (int i=0;i<size(distances);i++){
-    //    cout << distances[i] << endl;
-    //}    
-    save_map(window_height,window_width,&framebuffer);
-
-
-    draw_3d(window_height,window_width,&distances,&framebuffer);
-    save_map1(window_height,window_width,&framebuffer);
-    
-
-    //auto stop = chrono::high_resolution_clock::now();
-    //auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
-    //cout << duration.count();
 
     return 0;
 }
